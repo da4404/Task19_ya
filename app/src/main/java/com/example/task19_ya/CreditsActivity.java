0bf4ac8e05be921eacd998c7271d9e054837b678
בר אופק <u9054555@bsmch.net>
2026-03-21 15:42:49 +0200

    תפריט

--- app/src/main/java/com/example/task19_ya/CreditsActivity.java
+++ app/src/main/java/com/example/task19_ya/CreditsActivity.java
@@ -1,6 +1,9 @@
 package com.example.task19_ya;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,9 +14,37 @@ import androidx.core.view.WindowInsetsCompat;
 public class CreditsActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_game) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        else if (id == R.id.menu_settings) {
+            Intent intent = new Intent(this, setting.class);
+            startActivity(intent);
+            return true;
+        }
+        else if (id == R.id.menu_credits) {
+            Intent intent = new Intent(this, CreditsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/task19_ya/MainActivity.java
+++ app/src/main/java/com/example/task19_ya/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.task19_ya;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,5 +17,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_game) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        else if (id == R.id.menu_settings) {
+            Intent intent = new Intent(this, setting.class);
+            startActivity(intent);
+            return true;
+        }
+        else if (id == R.id.menu_credits) {
+            Intent intent = new Intent(this, CreditsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
